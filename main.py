384b165ac9ea474fb8e92741e91be296f5e7ada6
gespel <heimbrodt@uni-potsdam.de>
2025-02-06 22:52:02 +0100

    pprint and nicer data structure

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import requests
+import pprint
 from bs4 import BeautifulSoup
 
 URL = "https://www.spiegel.de/schlagzeilen/"
 page = requests.get(URL)
-#print(page.content)
 
 soup = BeautifulSoup(page.content, "html.parser")
-#data-area="article_teaser>news-s-wide"
+
+articles = []
+
 articles_cards = soup.find_all("div", {"data-area": "article_teaser>news-s-wide"})
 for article in articles_cards:
     #print(article)
     title = article.find("span", {"class": "mr-6"}).text
     link = article.find("a")["href"]
-    print(title)
-    print(link)
-#print(results.prettify())
+    articles.append({"title": title, "link": link})
+pprint.pprint(articles)
